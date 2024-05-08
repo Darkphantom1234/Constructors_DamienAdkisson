c119d90a3e1c9bf8eb66d16f26e8ac8a74d57998
Darkphantom <146202555+Darkphantom1234@users.noreply.github.com>
2024-05-08 10:48:27 -0500

    Initial commit

---
+++ Constructors_DamienAdkisson/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_DamienAdkisson
+{
+    internal class Game
+    {
+        //makes variables for use in the constructor
+        public string title, genre;
+
+        //sets default params
+        public Game(string titleParam = "Unkown Title", string genreParam = "Casual")
+        {
+            title = titleParam;
+            genre = genreParam;
+        }
+    }
+}
---
+++ Constructors_DamienAdkisson/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Constructors_DamienAdkisson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get and set my favorite game
+            Game myGame = new Game("Rainbow Six Siege", "FPS");
+            //print my fav game
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a(n) {myGame.genre} game!");
+            //grabs the default params 
+            Game myOtherGame = new Game();
+            //prints the defaults
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is a(n) {myOtherGame.genre}game!");
+
+            
+        }
+    }
+}
